dbad896afc416b2a93315c32a0a5acbea0f6e76d
Joe Tannenbaum <joe@joe.codes>
2020-11-21 09:38:15 -0500

    start to ordering by store

--- src/screens/ShoppingList.tsx
+++ src/screens/ShoppingList.tsx
@@ -10,20 +10,69 @@ import DraggableFlatList, {
 import { ShoppingListItem as ShoppingListItemType } from '@/types/ShoppingListItem';
 import { useQueryCache, useMutation } from 'react-query';
 import api from '@/api';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
+import useStores from '@/hooks/useStores';
+import BodyText from '@/components/BodyText';
+import { ScrollView } from 'react-native-gesture-handler';
+import { StoreTag } from '@/types/StoreTag';
 
 interface Props extends ScreenProps {
     id: number;
 }
 
-// https://reactnative.dev/docs/sectionlist
+interface ItemsByStoreTag {
+    tag: StoreTag;
+    items: ShoppingListItemType[];
+}
 
 const ShoppingList: Screen<Props> = (props) => {
+    const [activeStoreId, setActiveStoreId] = useState<number | null>(null);
+
     const list = useShoppingList(props.id);
+    const stores = useStores();
 
     const [listData, setListData] = useState(
         list.data?.active_version?.items || [],
     );
+
+    const [storeOrder, setStoreOrder] = useState<ItemsByStoreTag>([]);
+
+    useEffect(() => {
+        const orderedByStore = stores.data
+            ?.find((store) => store.id === activeStoreId)
+            ?.tags.map((tag) => {
+                return {
+                    tag,
+                    items: listData.filter((item) =>
+                        item.item.store_tags.find(
+                            (storeTag) => storeTag.id === tag.id,
+                        ),
+                    ),
+                };
+            })
+            .filter((tag) => tag.items.length > 0);
+
+        const uncategorizedItems = listData.filter(
+            (item) =>
+                !item.item.store_tags.find(
+                    (storeTag) => storeTag.store_id === activeStoreId,
+                ),
+        );
+
+        if (uncategorizedItems.length > 0) {
+            orderedByStore?.unshift({
+                tag: {
+                    name: 'Uncategorized',
+                    id: -1,
+                    store_id: -1,
+                    order: -1,
+                },
+                items: uncategorizedItems,
+            });
+        }
+
+        setStoreOrder(orderedByStore);
+    }, [listData, activeStoreId]);
 
     useEffect(() => {
         setListData(list.data?.active_version?.items || []);
@@ -55,24 +104,68 @@ const ShoppingList: Screen<Props> = (props) => {
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ padding: 20 }}>
+                {stores.data?.map((store) => (
+                    <TouchableOpacity
+                        onPress={() => {
+                            if (store.id === activeStoreId) {
+                                setActiveStoreId(null);
+                            } else {
+                                setActiveStoreId(store.id);
+                            }
+                        }}>
+                        <BodyText bold={store.id === activeStoreId}>
+                            {store.name}
+                        </BodyText>
+                    </TouchableOpacity>
+                ))}
+            </View>
             <View style={{ zIndex: 100 }}>
                 <CreateItemForm listId={props.id} />
             </View>
-            <DraggableFlatList
-                style={{ flex: 1 }}
-                data={listData}
-                keyExtractor={(item) => item.id.toString()}
-                onDragEnd={onDragEnd}
-                renderItem={({ item, drag, isActive }) => (
-                    <ShoppingListItem
-                        listId={props.id}
-                        item={item}
-                        key={item.id.toString()}
-                        drag={drag}
-                        isActive={isActive}
-                    />
-                )}
-            />
+            <ScrollView style={{ flex: 1 }}>
+                {activeStoreId &&
+                    storeOrder?.map((section) => (
+                        <View key={section.tag.id.toString()}>
+                            <BodyText bold={true}>{section.tag.name}</BodyText>
+                            {section.items.map((item) => (
+                                <ShoppingListItem
+                                    listId={props.id}
+                                    item={item}
+                                    key={item.id.toString()}
+                                    // drag={drag}
+                                    // isActive={isActive}
+                                />
+                            ))}
+                        </View>
+                    ))}
+                {!activeStoreId &&
+                    listData.map((item) => (
+                        <ShoppingListItem
+                            listId={props.id}
+                            item={item}
+                            key={item.id.toString()}
+                            // drag={drag}
+                            // isActive={isActive}
+                        />
+                    ))}
+                {/* <DraggableFlatList
+                    scrollEnabled={false}
+                    style={{ flex: 1 }}
+                    data={listData}
+                    keyExtractor={(item) => item.id.toString()}
+                    onDragEnd={onDragEnd}
+                    renderItem={({ item, drag, isActive }) => (
+                        <ShoppingListItem
+                            listId={props.id}
+                            item={item}
+                            key={item.id.toString()}
+                            drag={drag}
+                            isActive={isActive}
+                        />
+                    )}
+                /> */}
+            </ScrollView>
         </SafeAreaView>
     );
 };
