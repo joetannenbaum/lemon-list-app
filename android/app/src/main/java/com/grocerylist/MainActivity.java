454da75558bfc9236584ac2d89404288da4bdc71
Joe Tannenbaum <joe@joe.codes>
2020-11-28 07:56:20 -0500

    android!

--- android/app/src/main/java/com/grocerylist/MainActivity.java
+++ android/app/src/main/java/com/grocerylist/MainActivity.java
@@ -6,16 +6,7 @@ import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import android.content.Intent;
 
-public class MainActivity extends NavigationActivity {
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
-  }
+public class  MainActivity extends NavigationActivity {
 
   @Override
   public void onNewIntent(Intent intent) {
--- android/app/src/main/java/com/grocerylist/MainApplication.java
+++ android/app/src/main/java/com/grocerylist/MainApplication.java
@@ -1,14 +1,15 @@
 package com.grocerylist;
 
-import android.app.Application;
 import android.content.Context;
+
+import com.bugsnag.android.Bugsnag;
 import com.facebook.react.PackageList;
-import com.reactnativenavigation.NavigationApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
-import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
