52eed5f6b58b31e0f7403d829c2ade25e5c42864
Joe Tannenbaum <joe@joe.codes>
2021-03-02 21:15:13 -0500

    sloppy hit

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { showMenu } from '@/util/navigation';
-import { sizeImage } from '@/util/style';
+import { sizeImage, bsl } from '@/util/style';
 
 export interface MenuButtonProps {}
 
 const MenuButton: React.FC<MenuButtonProps> = (props) => {
     return (
-        <TouchableOpacity onPress={showMenu}>
+        <TouchableOpacity
+            onPress={showMenu}
+            hitSlop={{
+                top: bsl(20),
+                bottom: bsl(20),
+                left: bsl(20),
+                right: bsl(20),
+            }}>
             <Image style={styles.image} source={require('@images/menu.png')} />
         </TouchableOpacity>
     );
