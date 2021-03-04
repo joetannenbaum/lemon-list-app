322830412e320bd732a6de8bfdaa23d7ce4361ad
Joe Tannenbaum <joe@joe.codes>
2021-03-03 22:01:46 -0500

    limit results to 5

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -28,7 +28,9 @@ const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
         fuse.current = new Fuse(props.data, fuseOptions);
     }, [props.data]);
 
-    const results = fuse.current.search(props.query);
+    const results = fuse.current.search(props.query, {
+        limit: 5,
+    });
 
     if (props.query.trim().length < 2 || results.length === 0) {
         return null;
